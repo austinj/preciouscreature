1c0d9be71cd35a183c9acea5f240eca2ce99fce3
Austin Johnson <austinj@gmail.com>
2024-09-05 14:15:55 -0700

    coast gallery page is good, next working on nested layout for gallery

---
+++ src/scripts/lightbox.js
@@ -0,0 +1,23 @@
+import pswpModule from 'photoswipe'
+import PhotoSwipeLightbox from 'photoswipe/lightbox'
+import PhotoSwipeDynamicCaption from 'photoswipe-dynamic-caption-plugin';
+
+const lightbox = new PhotoSwipeLightbox({
+  pswpModule,
+  children: 'a',
+  gallery: '#my-gallery',
+  showHideAnimationType: 'fade',
+  imageClickAction: 'next',
+  tapAction: 'next',
+})
+
+const captionPlugin = new PhotoSwipeDynamicCaption(lightbox, {
+  // Plugins options, for example:
+  type: 'auto',
+});
+
+document.addEventListener(
+  'astro:page-load',
+  () => { if (lightbox) lightbox.init() },
+  { once: false },
+)
