f219aaa75a49ed496c3feaec7b69c7d287a5a654
Austin Johnson <austinj@gmail.com>
2024-09-08 23:16:25 -0700

    added sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,16 +5,16 @@ import netlify from "@astrojs/netlify";
 
 import partytown from "@astrojs/partytown";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      // Example: Disable injecting a basic `base.css` import on every page.
-      // Useful if you need to define and/or import your own custom `base.css`.
-      applyBaseStyles: false,
-    }),
-    partytown({ config: { forward: ["dataLayer.push"] } }),
-  ],
+  site: 'https://www.preciouscreaturetaxidermy.com',
+  integrations: [tailwind({
+    // Example: Disable injecting a basic `base.css` import on every page.
+    // Useful if you need to define and/or import your own custom `base.css`.
+    applyBaseStyles: false,
+  }), partytown({ config: { forward: ["dataLayer.push"] } }), sitemap()],
 
   output: "static",
   adapter: netlify(),
