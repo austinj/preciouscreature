0e57979512bcadffcec21583a7b6f4efb9d451d6
Austin Johnson <austinj@gmail.com>
2024-09-16 22:22:53 -0700

    index is now all one div, hooray

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,12 +9,16 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.preciouscreaturetaxidermy.com',
-  integrations: [tailwind({
-    // Example: Disable injecting a basic `base.css` import on every page.
-    // Useful if you need to define and/or import your own custom `base.css`.
-    applyBaseStyles: false,
-  }), partytown({ config: { forward: ["dataLayer.push"] } }), sitemap()],
+  site: "https://www.preciouscreaturetaxidermy.com",
+  integrations: [
+    tailwind({
+      // Example: Disable injecting a basic `base.css` import on every page.
+      // Useful if you need to define and/or import your own custom `base.css`.
+      applyBaseStyles: false,
+    }),
+    partytown({ config: { forward: ["dataLayer.push"] } }),
+    sitemap(),
+  ],
 
   output: "static",
   adapter: netlify(),
